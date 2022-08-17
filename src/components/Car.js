713baa6df7818f13a73722a4a2ccff6a4e5be32f
Roisin-13 <Roisin-13@users.noreply.github.com>
2022-08-17 16:12:45 +0100

    car factory app for state

---
+++ src/components/Car.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+const Car = () => {
+ return (
+    <>
+        <div className="car"></div>
+        <div className="car_carBody"></div>
+        <div className="car_wheel_front"></div>
+         <div className="car_wheel_back"></div>
+    </>
+ );
+}
+export default Car;
